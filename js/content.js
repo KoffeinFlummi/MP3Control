447dc8164df10cab86516b61aa4498e0af095dce
KoffeinFlummi <koffeinflummi@gmail.com>
2014-04-09 09:10:26 +0200

    Removed debug stuff

--- js/content.js
+++ js/content.js
@@ -1,5 +1,3 @@
-console.log("test");
-console.log($("body").children().length == 1 && $("body").children()[0].nodeName == "VIDEO" && $("source").attr("type") == "audio/mpeg");
 if ($("body").children().length == 1 && $("body").children()[0].nodeName == "VIDEO" && $("source").attr("type") == "audio/mpeg") {
   $("video").css("width", "80%");
 }
